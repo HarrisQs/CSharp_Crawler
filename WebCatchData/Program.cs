fed7667406a55f902862b58a33e6d3fa2b70555d
HarrisQs <harris32916@gmail.com>
2016-04-29 21:05:23 +0800

    add comment

--- WebCatchData/Program.cs
+++ WebCatchData/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿//2016/04/29
+//Programmer：張弘瑜
+//從網頁抓取上面的資料
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -18,21 +21,28 @@ namespace WebCatchData
         }
         private static void CreateWebRequest()
         {
-            //建立一個網頁連線並取得上面的資料
-            WebRequest request = WebRequest.Create("http://140.138.6.120/sampling.php");
-            WebResponse response = request.GetResponse();
-            Console.WriteLine("WebSite Response Status : " + ((HttpWebResponse)response).StatusDescription);
-            StreamReader reader = new StreamReader(response.GetResponseStream());
-            string responseFromServer = reader.ReadToEnd();
-            string jsonString = responseFromServer;
-            JObject json = JObject.Parse(jsonString);
-            String [] catchGoal = { "Co2", "Temperature", "Humidity" };
-            for (int i = 0; i < 3; i++) {
+            try
+            {
+                //建立一個網頁連線並取得上面的資料
+                WebRequest request = WebRequest.Create("http://140.138.6.120/sampling.php");
+                WebResponse response = request.GetResponse();
+                Console.WriteLine("WebSite Response Status : " + ((HttpWebResponse)response).StatusDescription);
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+                string responseFromServer = reader.ReadToEnd();
+                string jsonString = responseFromServer;
+                JObject json = JObject.Parse(jsonString);
+                String [] catchGoal = { "Co2", "Temperature", "Humidity" };
+                for (int i = 0; i < 3; i++) {
 
-                SaveFile(catchGoal[i]+".txt", json[catchGoal[i]].ToString());
+                    SaveFile(catchGoal[i]+".txt", json[catchGoal[i]].ToString());
+                }
+            }
+            catch (WebException e)//如果沒連上網頁的話
+            {
+                Console.WriteLine("Exception thrown.\nThe Original Message is: " + e.Message);
             }
         }
-        private static void SaveFile(String filaName, String Data)
+        private static void SaveFile(String filaName, String Data)//把資料存成檔案
         {
             using (StreamWriter outputFile = new StreamWriter(filaName))
             {
